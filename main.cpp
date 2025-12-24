14786e79336d6884d6ea3d74419edfda6fce7963
JeiKarousel <theconfid3ntialz@gmail.com>
2025-12-24 13:49:16 +0800

    put the functions from main to functions header

--- main.cpp
+++ main.cpp
@@ -24,50 +24,6 @@ bool isRogoatuskan = false;
 bool isCrew = false;
 bool isShips = false;
 
-bool check_File_isCSV(const string filename)
-{
-    if (!(filename.substr(filename.size() - 4) == ".csv"))
-    {
-        cout << "Invalid type of file! " << endl;
-        return 1;
-    }
-}
-
-bool check_Sides(const string filename)
-{
-    if (filename.size() <= 4)
-    {
-        cout << "Invalid filename!" << endl;
-    }
-
-    if (filename[0] == 'z')
-    {
-        return isZapezoid = true;
-    }
-    else if (filename[0] == 'r')
-    {
-        return isRogoatuskan = true;
-    }
-    else
-    {
-        cout << "Invalid name of file! Please specify type of ship/crew! " << endl;
-        return 1;
-    }
-
-    if (filename.substr(1, (filename.size() - 4)) == "Ships" || filename.substr(1, (filename.size() - 5)) == "Ships")
-    {
-        isShips = true;
-    }
-    else if (filename.substr(1, (filename.size() - 4)) == "Crew" || filename.substr(1, (filename.size() - 5)) == "Crew")
-    {
-        isCrew = true;
-    }
-    else
-    {
-        cout << "Invalid filename!" << endl;
-        return 1;
-    }
-}
 
 int main(int argc, char *argv[])
 {
