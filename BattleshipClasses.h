f528f06c2bcc7200c3bac2091562479c88c71fa2
OrpheusJaso <jasonwan9937@gmail.com>
2026-01-16 20:15:09 +0800

    Added Zapezoid Ships

--- BattleshipClasses.h
+++ BattleshipClasses.h
@@ -6,6 +6,34 @@
 
 using namespace std;
 
+/////////////////////////////////////// ALL CREWS
+class crewHolder{
+    protected: 
+    string crewType, crewName, crewID;
+
+    crewHolder(string a, string b){
+
+        a = crewID;
+        b = crewName;
+    }
+};
+
+class pilot : public crewHolder{
+
+    public:
+    pilot(string CREW_ID, string CREW_name) : crewHolder(CREW_ID, CREW_name) {}
+};
+
+class gunner : public crewHolder{
+    public:
+    gunner(string CREW_ID, string CREW_name) : crewHolder(CREW_ID, CREW_name) {}
+};
+
+class torpedohandler : public crewHolder{
+    public:
+    torpedohandler(string CREW_ID, string CREW_name) : crewHolder(CREW_ID, CREW_name) {}
+};
+
 // Base Initialisation of the Battleship format
 class Battleships{
     protected:
@@ -43,25 +71,11 @@ class Battleships{
         }
 };
 
-// EMPTY CLASSES JUST AS PLACEHOLDERS
-/////////////////////////////// MAIN PARENT CLASS
-class shipHolder{
-
-    public: 
-    string shipType, shipName, shipID;
-
-    shipHolder(string a, string b){
-
-        a = shipID;
-        b = shipName;
-    }
-};
-
 /////////////////////////////// ALL ARE Z
 class Guerriero : public Battleships{
 
     public:
-    Guerriero(int hp, string id, string name) : Battleships(hp, id, name){
+    Guerriero(int hp, string id, string name) : Battleships(123, id, name){
         short hitByCannon = 26;
         short hitByTorpedo = 6;
         lightCannon.power = 96;
@@ -70,59 +84,45 @@ class Guerriero : public Battleships{
     }
 };
 
-class Medio : public shipHolder{
+class Medio : public Battleships{
       
     public:
-    Medio(string SHIP_ID, string SHIP_name) : shipHolder( SHIP_ID, SHIP_name) {}
+    Medio(int hp, string id, string name) : Battleships(214, id, name){
+        short hitByCannon = 31;
+        short hitByTorpedo = 11;
+        lightCannon.power = 134;
+        lightCannon.amount = 2;
+        shipCrew(1,2,0);
+    }
 };
-class Corazzata : public shipHolder{
+class Corazzata : public Battleships{
       
     public:
-    Corazzata(string SHIP_ID, string SHIP_name) : shipHolder( SHIP_ID, SHIP_name) {}
-};
-
-
-/////////////////////////////////// ALL ARE R
-class Jager : public shipHolder{
-    public:
-    Jager(string SHIP_ID, string SHIP_name) : shipHolder( SHIP_ID, SHIP_name) {}
-};
-
-class Kreuzer : public shipHolder{
-    public:
-    Kreuzer(string SHIP_ID, string SHIP_name) : shipHolder( SHIP_ID, SHIP_name) {}
-};
-class Fregatte : public shipHolder{
-    public:
-    Fregatte(string SHIP_ID, string SHIP_name) : shipHolder( SHIP_ID, SHIP_name) {}
-};
-
-
-/////////////////////////////////////// ALL CREWS
-class crewHolder{
-    public: 
-    string crewType, crewName, crewID;
-
-    crewHolder(string a, string b){
-
-        a = crewID;
-        b = crewName;
+    Corazzata(int hp, string id, string name) : Battleships(1031, id, name){
+        short hitByCannon = 50;
+        short hitByTorpedo = 25;
+        lightCannon.power = 164;
+        lightCannon.amount = 10;
+        torpedo.power = 293;
+        torpedo.amount = 4;
+        shipCrew(2,10,4);
     }
 };
 
-class pilot : public crewHolder{
-
+/////////////////////////////////// ALL ARE R
+class Jager : public Battleships{
     public:
-    pilot(string CREW_ID, string CREW_name) : crewHolder(CREW_ID, CREW_name) {}
+    Jager(int hp, string id, string name) : Battleships(123, id, name) {}
 };
 
-class gunner : public crewHolder{
+class Kreuzer : public Battleships{
     public:
-    gunner(string CREW_ID, string CREW_name) : crewHolder(CREW_ID, CREW_name) {}
+    Kreuzer(int hp, string id, string name) : Battleships(214, id, name) {}
+};
+class Fregatte : public Battleships{
+    public:
+    Fregatte(int hp, string id, string name) : Battleships(1031, id, name) {}
 };
 
-class torpedohandler : public crewHolder{
-    public:
-    torpedohandler(string CREW_ID, string CREW_name) : crewHolder(CREW_ID, CREW_name) {}
-};
+
 
